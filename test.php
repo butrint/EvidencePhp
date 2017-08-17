bc63c4c5d13be13dc6f4b41a0cecf425302389de
butrint <buti.5@hotmail.com>
2017-08-17 13:24:26 +0200

    test

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
+	include("config.php");
 	$name = "Butrint";
 	$surname = "Rashiti";
+	if(isset($_GET["id"]))
+	{
+		$sql = "SELECT * FROM students WHERE `id_student`='13'";
+		$result = mysqli_query($conn,$sql);
+		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+		
+		return $row;
+	}
 ?>
