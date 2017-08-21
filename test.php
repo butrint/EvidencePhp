b65438c679eaa8bb95577b26852a8d3487afd72b
butrint <buti.5@hotmail.com>
2017-08-21 17:28:24 +0200

    update

--- test.php
+++ test.php
@@ -3,7 +3,8 @@
         $surname = "Rashiti";
         if(isset($_GET["id"]))
         {
-                $sql = "SELECT * FROM students WHERE `num_id`='13'";
+		   		$num_id = $_GET["id"];
+                $sql = "SELECT * FROM students WHERE `num_id`='".$id."'";
                 $result = mysqli_query($conn,$sql);
                 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 
