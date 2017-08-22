ac42af3c371ae1db80acf784867df0752a593f05
butrint <buti.5@hotmail.com>
2017-08-22 15:10:49 +0200

    update

--- test.php
+++ test.php
@@ -3,7 +3,16 @@
         $surname = "Rashiti";
         if(isset($_GET["id"]))
         {
-		   		$num_id = $_GET["id"];
+			  $to      = 'buti.5@hotmail.com';
+			  $subject = 'the subject';
+			  $message = 'hello';
+			  $headers = 'From: webmaster@example.com' . "\r\n" .
+				  'Reply-To: webmaster@example.com' . "\r\n" .
+				  'X-Mailer: PHP/' . phpversion();
+			  mail($to, $subject, $message, $headers);
+			  
+			  
+			  $num_id = $_GET["id"];
                 $sql = "SELECT * FROM students WHERE `num_id`='".$id."'";
                 $result = mysqli_query($conn,$sql);
                 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
