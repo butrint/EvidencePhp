970ecebaf2dfcf95275134a913625242b57fdfcd
butrint <buti.5@hotmail.com>
2017-09-02 22:19:42 +0200

    Nuk lejon riregjistrim

--- setStudPassword.php
+++ setStudPassword.php
@@ -12,45 +12,49 @@
 		
 		$sql = "SELECT * FROM `students` WHERE `num_id`='".$num_id."' AND `email`='".$email."' LIMIT 1";
 		$result = $conn->query($sql);
-		echo "result";
 		// Kontrollon nese ka t'dhena ndatabaze per at student
 		if(($result) && mysqli_num_rows($result))
 		{
-			echo "mas result";
 			// Dergon imellen dhe regjistron passwordin me ate student
 			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 			
 			mysqli_free_result($result);
 			
-			$studPass = "1234567";
-			$md5StudPass = md5($studPass);
-			$from     = 'evidenca@noreply.com';
-			$to       = $email;
-			$subject  = 'Evidenca';
-
-			$body = 'Passwordi juaj eshte: '. $studPass;
-			
-			$table = "devicereg(password)";
-			$values = "'".$md5StudPass."'";
-			$lastDevId = insertIntoDbs($conn, $table, $values);
-			echo $lastDevId;
-			if(is_numeric($lastDevId))
+			if($row["devicereg_id"] === NULL)
 			{
-				if(sendMail($from, $to, $subject, $body))
-				{
-					$table = "students";
-					$set = "devicereg_id='".$lastDevId."'";
-					$additional = "WHERE num_id='".$num_id."'";
-					updateDbs($conn, $table, $set, $additional);
-				}
+				$studPass = "1234567";
+				$md5StudPass = md5($studPass);
+				$from     = 'evidenca@noreply.com';
+				$to       = $email;
+				$subject  = 'Evidenca';
 
-				return json_encode($row);
+				$body = 'Passwordi juaj eshte: '. $studPass;
+
+				$table = "devicereg(password)";
+				$values = "'".$md5StudPass."'";
+				$lastDevId = insertIntoDbs($conn, $table, $values);
+				if(is_numeric($lastDevId))
+				{
+					if(sendMail($from, $to, $subject, $body))
+					{
+						$table = "students";
+						$set = "devicereg_id='".$lastDevId."'";
+						$additional = "WHERE num_id='".$num_id."'";
+						updateDbs($conn, $table, $set, $additional);
+					}
+
+					// nese du me qu si JSON
+					// return json_encode($row);
+				}
 			}
+			else
+				return "Studenti/ja eshte regjistruar tashme deshironi te dergojme nje fjalekalim te ri?";
+			
 		}
 		else
 		{
 			// Can be a problem with connection to database or doesn't have data
-			echo "Nuk mundi te behej lidhja ose nuk ka te dhena";
+			echo "Lidhja nuk mund te behet ose nuk ka te dhena";
 		}
   	}
 	
