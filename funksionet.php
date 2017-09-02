d17a8d34c25094827003b6aa75fc1f659874dae9
butrint <buti.5@hotmail.com>
2017-09-02 19:15:21 +0200

    students password funksionet

---
+++ funksionet.php
@@ -0,0 +1,105 @@
+<?php
+	require_once 'Mail.php';
+	
+	//returns the id of the inserted row
+	function insertIntoDbs($conn, $table, $values)
+	{
+		$id="";
+		$sql = "INSERT INTO ".$table." VALUES(".$values.");";
+		$result = $conn->query($sql);
+		if($result === true)
+		{
+			$id = mysqli_insert_id($conn);
+			return $id;
+		}
+		else
+		{
+			return "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+
+	function updateDbs($conn, $table, $set, $additional="")
+	{
+		$sql = 
+		$sql = "";
+		if($additional != "")
+			$sql = "UPDATE ".$table." SET ".$set." ".$additional."";
+		else
+			$sql = "UPDATE ".$table." SET ".$set."";
+		
+		$result = $conn->query($sql);
+		if(($result === true) && (mysqli_num_rows($result) > 0))
+		{
+			return "Record updated successfully";	
+		}
+		else
+		{
+			return "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+
+	// Kthen array te dyfisht ($res[0]['name']) sepse rezultatet mund te ken me shum se 1 rresht
+	function selectFromDbs($conn, $table, $select, $additional="")
+	{
+		$sql = "";
+		if($additional != "")
+			$sql = "SELECT ".$select." FROM ".$table." ".$additional."";
+		else
+			$sql = "SELECT ".$select." FROM ".$table."";
+		$result = $conn->query($sql);
+		
+		if(($result === true) && (mysqli_num_rows($result) > 0))
+		{
+			$res = array();
+			while($row = mysqli_fetch_assoc($result))
+			{
+				$res[] = $row;
+			}
+			return $res;
+		}
+		else
+		{
+			// Kthen false nese nuk ka affected rows
+			if((mysqli_num_rows($result) == 0))
+				return false;
+			return "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+	
+	function sendMail($from, $to, $subject, $body)
+	{
+		$host = "smtp.gmail.com";
+		$username = "evidencaproject@gmail.com";
+		$mailPassword = "Jimmy123";
+
+		$recipients = 'evidencaproject@gmail.com';
+		
+		$params['sendmail_path'] = '/usr/lib/sendmail';
+		
+		
+		$headers = array ('From' => $from,
+				 'To' => $to,
+				 'Subject' => $subject);
+		
+		$smtp = Mail::factory('smtp',
+						array ('host' => $host,
+							'port' => 587,
+							'auth' => true,
+							'username' => $username,
+							'password' => $mailPassword));
+
+		$mail = $smtp->send($to, $headers, $body);
+
+		if (PEAR::isError($mail))
+		{
+			//echo "error: {$mail->getMessage()}"; 
+			return false;
+		}
+		else 
+		{
+			return true;
+			echo "Message sent";
+		}
+	}
+
+?>
---
+++ setStudPassword.php
@@ -0,0 +1,53 @@
+<?php
+	include "./config.php";
+	include "./funksionet.php";
+	
+	if(isset($_GET["id"]) && isset($_GET["email"]))
+	{
+		$num_id = $_GET["id"];
+		$email = $_GET["email"];
+		
+		$sql = "SELECT * FROM `students` WHERE `num_id`='".$num_id."' AND `email`='".$email."' LIMIT 1";
+		$result = $conn->query($sql);
+		// Kontrollon nese ka t'dhena ndatabaze per at student
+		if(($result) && mysqli_num_rows($result))
+		{
+			// Dergon imellen dhe regjistron passwordin me ate student
+			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			mysqli_free_result($result);
+			
+			$studPass = "1234567";
+			$md5StudPass = md5($studPass);
+			$from     = 'evidenca@noreply.com';
+			$to       = $email;
+			$subject  = 'Evidenca';
+
+			$body = 'Passwordi juaj eshte: '. $studPass;
+			
+			$table = "devicereg(password)";
+			$values = $md5StudPass;
+			$lastDevId = insertIntoDbs($conn, $table, $values);
+			
+			if(is_numeric($lastDevId))
+			{
+				if(sendMail($from, $to, $subject, $body))
+				{
+					$table = "students";
+					$set = "devicereg_id='".$lastDevId."'";
+					$additional = "WHERE num_id='".$num_id."'";
+					updateDbs($conn, $table, $set, $additional);
+				}
+
+				return json_encode($row);
+			}
+		}
+		else
+		{
+			// Can be a problem with connection to database or doesn't have data
+			echo "Nuk mundi te behej lidhja ose nuk ka te dhena";
+		}
+  	}
+	
+
+?>
+
