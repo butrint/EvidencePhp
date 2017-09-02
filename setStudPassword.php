9cc0f44df2c2d3a7324e9f3350da31725a565c43
root <root@instance-1.c.evidenca-176411.internal>
2017-09-02 19:50:26 +0000

    setPass updated

--- setStudPassword.php
+++ setStudPassword.php
@@ -2,18 +2,24 @@
 	include "./config.php";
 	include "./funksionet.php";
 	
+	echo "fillim";
+	
 	if(isset($_GET["id"]) && isset($_GET["email"]))
 	{
+		echo "isset";
 		$num_id = $_GET["id"];
 		$email = $_GET["email"];
 		
 		$sql = "SELECT * FROM `students` WHERE `num_id`='".$num_id."' AND `email`='".$email."' LIMIT 1";
 		$result = $conn->query($sql);
+		echo "result";
 		// Kontrollon nese ka t'dhena ndatabaze per at student
 		if(($result) && mysqli_num_rows($result))
 		{
+			echo "mas result";
 			// Dergon imellen dhe regjistron passwordin me ate student
 			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			
 			mysqli_free_result($result);
 			
 			$studPass = "1234567";
@@ -25,9 +31,9 @@
 			$body = 'Passwordi juaj eshte: '. $studPass;
 			
 			$table = "devicereg(password)";
-			$values = $md5StudPass;
+			$values = "'".$md5StudPass."'";
 			$lastDevId = insertIntoDbs($conn, $table, $values);
-			
+			echo $lastDevId;
 			if(is_numeric($lastDevId))
 			{
 				if(sendMail($from, $to, $subject, $body))
